719c62ec372c70d8e55c7b7abb980d6767a6548f
tiwavaldese <tiwaval7@gmail.com>
2024-04-23 14:19:04 +0100

    add comment in the profile statistics project

--- projects/profile-statistics/index.js
+++ projects/profile-statistics/index.js
@@ -1,12 +1,14 @@
 const countersEl = document.querySelectorAll(".counter");
-
+//Iterate over each elements in the counter
 countersEl.forEach((counterEl) => {
+  //Initial value of the counter to zero
   counterEl.innerText = "0";
   incrementCounter();
   function incrementCounter() {
     let currentNum = +counterEl.innerText;
     const dataCeil = counterEl.getAttribute("data-ceil");
     const increment = dataCeil / 15;
+    //Math.ceil()= round to the nearest integer
     currentNum = Math.ceil(currentNum + increment);
 
     if (currentNum < dataCeil) {
